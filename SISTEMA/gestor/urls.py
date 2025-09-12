b6eda7e705cf8ff73fbfd2e217beb0f11de5b782
Paudani1029 <pauladanielasaavedra2005@gmail.com>
2025-09-12 16:12:46 -0300

    portal pacientes

--- SISTEMA/gestor/urls.py
+++ SISTEMA/gestor/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.inicio, name='index'),
-    path('login/',views.login, name='login')
+    path('login/',views.login, name='login'),
+    path('reservas/',views.reservas, name='reservas'),
+    path('PortalPacientes/',views.PortalPacientes, name='PortalPacientes'),
+    path('login/PortalPacientes/', views.PortalPacientes, name='PortalPacientes'),
+    path('login/reservas/', views.reservas, name='reservar_cita')
 ]
 
--- SISTEMA/gestor/views.py
+++ SISTEMA/gestor/views.py
@@ -5,3 +5,9 @@ def inicio(request):
 
 def login(request):
     return render(request, 'gestor/login.html')
+
+def reservas(request):
+    return render(request, 'gestor/reservar-cita.html')
+
+def PortalPacientes(request):
+    return render(request, 'gestor/portal-pacientes.html')
